bb2689740707c0d21e56bd27bc9c51ebc512381d
Joseph_Bio <josephbiossey03@gmail.com>
2025-09-15 12:39:11 +0000

    Add python_introduction directory

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,12 @@
+# A program to convert hours into seconds
+
+hours = 2     # I defined a user-defined variable for hours
+
+
+# formula to convert hours into seconds
+seconds = hours * 3600
+
+
+# This print the result
+print(f"{hours} hours is {seconds} seconds.")
+
