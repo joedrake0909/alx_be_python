49b2e3da0f55b032aebf0b208f8beca7f1214c68
Joseph_Bio <josephbiossey03@gmail.com>
2025-09-15 04:26:51 +0000

    This is week three practical sesssion held by ALX

---
+++ future_age_calculator.py
@@ -0,0 +1,12 @@
+# A program that calculates your future age based on your current age and a number of years in the future.
+
+current_age = input("How old are you? ")      # This prompt the user to input their current age
+
+age_in_27_years = int(current_age) + 27      # This calculate the future age by adding 27 to the current age
+
+
+# This print the result
+print(f"In 2050 upu will be {age_in_27_years} years old.")
+
+
+
