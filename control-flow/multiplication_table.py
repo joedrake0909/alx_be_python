df3c1a60bb489a65a4dde5eb791920abd2e773a9
Joseph_Bio <josephbiossey03@gmail.com>
2025-09-21 23:48:22 +0000

    This is the corrected version of introduction to control flow

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,9 @@
+# Multiplication Table Generator
+
+# Prompt user for a number
+number = int(input("Enter a number to see its multiplication table: "))
+
+# Generate and print multiplication table using for loop
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} * {i} = {result}")
